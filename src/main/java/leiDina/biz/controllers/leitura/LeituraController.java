29bc673031efdf97c566692b0e6ee74d6a2d41c0
vitor.alves <vitors.alves@hotmail.com>
2020-04-06 20:53:12 -0300

    Logic for dynamic reading implemented.

--- src/main/java/leiDina/biz/controllers/leitura/LeituraController.java
+++ src/main/java/leiDina/biz/controllers/leitura/LeituraController.java
@@ -2,15 +2,27 @@ package main.java.leiDina.biz.controllers.leitura;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
+import main.java.leiDina.tec.core.utils.StringUtils;
 import main.java.leiDina.tec.javafx.controller.BaseModelController;
 
 /**
  * @author vitor.alves
  */
 public class LeituraController extends BaseModelController<LeituraModel> {
+
+    private static String[] text;
+
+    private static int next = 0;
+
+    private static String fullText = "";
 
     @FXML
     private TextArea textoSaida;
@@ -28,6 +40,25 @@ public class LeituraController extends BaseModelController<LeituraModel> {
 
     @FXML
     public void lerTexto(MouseEvent mouseEvent) {
-        // Todo: add logica para a leitura.
+        String msg = this.getModel().getTextoEntrada();
+        if (StringUtils.isNotEmpty(msg)) {
+            LeituraController.text = msg.split("\\W+");;
+            Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0.5), new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    String nextString = LeituraController.getNextString();
+                    textoSaida.setText(nextString);
+                }
+            }));
+            timeline.setCycleCount(text.length);
+            timeline.play();
+        }
+    }
+
+    private static String getNextString() {
+        String s = text[next];
+        next++;
+        fullText = fullText + " " + s;
+        return fullText;
     }
 }
