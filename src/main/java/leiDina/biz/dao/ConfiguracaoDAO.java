0f517cd573b2077815e0180e5978b7e77b79cc45
vitor.alves <vitors.alves@hotmail.com>
2020-04-12 16:23:13 -0300

    Adding constructor to ConfiguracaoDAO.

--- src/main/java/leiDina/biz/dao/ConfiguracaoDAO.java
+++ src/main/java/leiDina/biz/dao/ConfiguracaoDAO.java
@@ -1,12 +1,17 @@
 package main.java.leiDina.biz.dao;
 
 import main.java.leiDina.biz.model.Configuracao;
+import main.java.leiDina.tec.persister.Persister;
 import main.java.leiDina.tec.persister.dao.BaseDAO;
 
 /**
  * @author vitor.alves
  */
 public class ConfiguracaoDAO extends BaseDAO<Configuracao> {
+
+    public ConfiguracaoDAO(Persister persister) {
+        super(persister);
+    }
 
     @Override
     protected Class<Configuracao> getPersistableType() {
