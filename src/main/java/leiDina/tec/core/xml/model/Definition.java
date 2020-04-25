4724d5bddb8a07fc8bd487c7ebc999ef05cec352
vitor.alves <vitors.alves@hotmail.com>
2020-04-25 13:17:59 -0300

    CONFIG - Removing Packages.java.

--- src/main/java/leiDina/tec/core/xml/model/Definition.java
+++ src/main/java/leiDina/tec/core/xml/model/Definition.java
@@ -2,11 +2,9 @@ package main.java.leiDina.tec.core.xml.model;
 
 import java.util.List;
 import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 import main.java.leiDina.tec.core.xml.model.bean.Beans;
-import main.java.leiDina.tec.core.xml.model.packages.Packages;
 
 /**
  * @author vitor.alves
